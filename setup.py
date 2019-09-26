ebab7441cdf084c34241f2d8a70e5fb67f479a49
Stavros Korokithakis <hi@stavros.io>
2019-09-26 18:37:03 +0300

    Unconfuse myself

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from setuptools import setup
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname), "rb").read().decode("utf8")
 
 
 setup(
     name="django-admincommand",
     version="0.1.6",
     description="Execute management commands from the Django admin",
-    long_description=read("README.md", "rb").decode("utf8"),
+    long_description=read("README.md"),
     author="Anto59290",
     url="https://github.com/BackMarket/django-admincommand",
     packages=find_packages(),
